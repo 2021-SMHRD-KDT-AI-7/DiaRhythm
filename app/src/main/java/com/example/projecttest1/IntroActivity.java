6473e66bdc31e4e708edc367e6c0bfdd4500f877
박준영 <91214008+DevJYP@users.noreply.github.com>
2022-01-20 16:43:42 +0900

    2차 커밋

--- app/src/main/java/com/example/projecttest1/IntroActivity.java
+++ app/src/main/java/com/example/projecttest1/IntroActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class IntroActivity extends AppCompatActivity {
+
+    ImageView img_loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        img_loading = findViewById(R.id.img_loading);
+        Glide.with(this).load(R.drawable.loading_image).into(img_loading);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -22,7 +29,7 @@ public class IntroActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },2500);
 
 
         }
