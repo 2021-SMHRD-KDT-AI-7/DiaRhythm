e56be557674ba9292f5c6218f662af98439749e3
chan <jsj8017@naver.com>
2022-01-24 11:01:24 +0900

    chanjun2

--- app/src/main/java/com/example/projecttest1/Fragment2.java
+++ app/src/main/java/com/example/projecttest1/Fragment2.java
@@ -1,5 +1,6 @@
 package com.example.projecttest1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,46 +9,74 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+//import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Fragment2 extends Fragment {
-
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment2() {
-    }
-
-
-
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    LineChart lineChart;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View v = inflater.inflate(R.layout.fragment_2, container, false);
+        lineChart = (LineChart) v.findViewById(R.id.linechart);
+
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+
+
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+        entries.add(new Entry(16f, 6));
+
+        LineDataSet dataset = new LineDataSet(entries, "감정 분석");
+
+        List<String> labels = new ArrayList<String>();
+        labels.add("월");
+        labels.add("화");
+        labels.add("수");
+        labels.add("목");
+        labels.add("금");
+        labels.add("토");
+        labels.add("일");
+        LineData data = new LineData( labels,dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        /*dataset.setDrawCubic(true); //선 둥글게 만들기
+        dataset.setDrawFilled(true); //그래프 밑부분 색칠*/
+
+        lineChart.setData(data);
+        lineChart.animateY(3000);
+
+
+        return v;
+
+
     }
+
 }
+
+
+
+
+
+
+
+
