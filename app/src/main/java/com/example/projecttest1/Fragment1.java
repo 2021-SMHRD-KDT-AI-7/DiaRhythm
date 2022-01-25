84dddf4c3325ed95c5a856e71a4813c8bec4f3fb
chan <jsj8017@naver.com>
2022-01-25 17:02:05 +0900

    DiaryActivity 만들고 fragment1 에다가 글쓰기 이미지 넣고 다이어리엑팁 연결까지

--- app/src/main/java/com/example/projecttest1/Fragment1.java
+++ app/src/main/java/com/example/projecttest1/Fragment1.java
@@ -1,50 +1,43 @@
 package com.example.projecttest1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment1 extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment1() {
-    }
-
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    ImageView img_write;
+    ImageView jo;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        View v =inflater.inflate(R.layout.fragment_1, container, false);
+
+//        jo = v.findViewById(R.id.jo);
+        img_write = v.findViewById(R.id.img_write);
+
+        img_write.setOnClickListener(new View.OnClickListener() {
+          @Override
+            public void onClick(View view) {
+
+               Intent intent = new Intent(getContext(),DiaryActivity.class);
+               startActivity(intent);
+
+         }
+       });
+        return v;
+
+
+
+
+
     }
 }
