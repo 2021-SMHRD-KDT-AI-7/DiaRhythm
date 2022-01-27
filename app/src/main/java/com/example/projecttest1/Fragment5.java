36bb3c3841461cf36ce083107b27828d59c3a4a9
chan <jsj8017@naver.com>
2022-01-27 17:16:33 +0900

    내정보만듬

--- app/src/main/java/com/example/projecttest1/Fragment5.java
+++ app/src/main/java/com/example/projecttest1/Fragment5.java
@@ -7,15 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 
 public class Fragment5 extends Fragment {
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_5, container, false);
+
+    final ScrollView scrollView = (ScrollView)v.findViewById(R.id.sc_view);
+
+    scrollView.post(new Runnable() {
+        @Override
+        public void run() {
+            scrollView.fullScroll(scrollView.FOCUS_DOWN);
+        }
+    });
+
+
 
 
 
