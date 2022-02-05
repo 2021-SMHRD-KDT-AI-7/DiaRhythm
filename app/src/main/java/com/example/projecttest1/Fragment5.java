0d72ccb3ee0bd50b0b0d42734d07a70f9faa9c42
박준영 <91214008+DevJYP@users.noreply.github.com>
2022-02-05 15:33:57 +0900

    fragment5 일기 횟수 count commit

--- app/src/main/java/com/example/projecttest1/Fragment5.java
+++ app/src/main/java/com/example/projecttest1/Fragment5.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,8 @@ public class Fragment5 extends Fragment {
 
     ListView lv;
     TextView tv_ga, tv_name_f5,tv_join_f5,tv_grade,btn_logout;
-
+    String result;
+    String result_rep;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,12 +39,29 @@ public class Fragment5 extends Fragment {
 
         ArrayAdapter adapter = new ArrayAdapter(getActivity(),R.layout.info,info);
         lv.setAdapter(adapter);
+        try {
+            RegisterInfoActivity task = new RegisterInfoActivity();
 
-        info.add("내가 쓴 일기 횟수");
+            Bundle bundle = getArguments();
+
+            String id = bundle.getString("id");
+
+            result = task.execute(id).get();
+
+            Log.v("MY", result);
+            result_rep = result.replace(" ", "");
+            Log.v("일기쓴횟수", result_rep);
+
+        }catch (Exception e){
+
+        }
+
+        info.add("내가 쓴 일기 횟수" + result);
         info.add("연속 작성 횟수");
         info.add("공지사항");
         info.add("개인정보약관동의");
         info.add("앱버전");
+
 
 
         adapter.notifyDataSetChanged();
