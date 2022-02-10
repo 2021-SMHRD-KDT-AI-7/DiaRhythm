54e8b73c5efbde541c05948e8aeb0f7e42ee07a4
junyo <junyoung7798@gmail.com>
2022-02-10 17:46:00 +0900

    찐찐찐찐막 최종 커밋
    고생했다!

--- app/src/main/java/com/example/projecttest1/DiaryActivity.java
+++ app/src/main/java/com/example/projecttest1/DiaryActivity.java
@@ -180,7 +180,7 @@ public class DiaryActivity extends AppCompatActivity {
         //다이얼로그 텍스트 설정
         builder.setView(view);
         ((TextView)view.findViewById(R.id.textTitle)).setText("AI Comment");
-        ((TextView)view.findViewById(R.id.textMessage)).setText("눈물 닦아주고싶네요");
+        ((TextView)view.findViewById(R.id.textMessage)).setText("그동안 고생하셨어요");
         ((Button)view.findViewById(R.id.btnOk)).setText("확인");
 
         AlertDialog alertDialog = builder.create();
